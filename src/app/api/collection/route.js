5a08595802c4ce1ef5f2c5ad6e2c07b253c09fd1
Sadiq Naqvi <naqvisadiq6@gmail.com>
2024-09-16 14:30:01 +0530

    Added collection route

---
+++ src/app/api/collection/route.js
@@ -0,0 +1,21 @@
+export const GET = async (req) => {
+  const params = req.nextUrl.searchParams;
+  const collection_id = params.get("id");
+  const url = `https://api.themoviedb.org/3/collection/${collection_id}?language=en-US`;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.TMDB_API}`,
+    },
+  };
+
+  try {
+    const resp = await fetch(url, options);
+    const data = await resp.json();
+    return new Response(JSON.stringify({ status: true, response: data }));
+  } catch (err) {
+    console.error(err);
+    return new Response(JSON.stringify({ status: false }));
+  }
+};
