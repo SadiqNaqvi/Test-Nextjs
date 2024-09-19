326fe673140db09f01b281355749792c6079498b
Sadiq Naqvi <naqvisadiq6@gmail.com>
2024-09-19 20:54:20 +0530

    Added cast route

--- src/app/api/movies/route.js
+++ src/app/api/movies/route.js
@@ -2,14 +2,16 @@ import { movie_sort_obj as sortObj } from "@/utils/Data";
 export const GET = async (req) => {
   const params = req.nextUrl.searchParams;
   const genres = params.get("g");
+  const cast = params.get("c");
   const sort = params.get("sort") || "popularity";
   const sort_by = sortObj[sort] || sortObj.popularity;
   const page = params.get("p") || 1;
   const year = params.get("p");
 
-  const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=${sort_by}${
-    genres ? `&with_genres=${genres}` : ""
-  }${year ? `&with_year=${year}` : ""}`;
+  const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=${sort_by}
+  ${genres ? `&with_genres=${genres}` : ""}
+    ${year ? `&with_year=${year}` : ""}
+  ${cast ? `&with_cast=${cast}` : ""}`;
 
   const options = {
     method: "GET",
