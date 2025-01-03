430a159689ffbe4897ba133fc5461d89fdad2475
Sadiq Naqvi <naqvisadiq6@gmail.com>
2025-01-03 14:02:08 +0530

    Something is added

---
+++ src/app/api/adult/[username]/patreon/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (
+  res: NextRequest,
+  { params: { username } }: { params: { username: string } }
+) => {
+  const page = res.nextUrl.searchParams.get("o");
+
+  try {
+    const response = await fetch(
+      `https://kemono.su/api/v1/patreon/user/${username}/posts-legacy?o=${page}`
+    ).then((res) => res.json());
+
+    if (response.error)
+      return NextResponse.json({
+        result: null,
+        success: false,
+        error: response.error,
+      });
+
+    return NextResponse.json({
+      result: response,
+      success: true,
+      error: null,
+    });
+  } catch (err: any) {
+    return NextResponse.json({
+      result: null,
+      success: false,
+      error: err.message,
+    });
+  }
+};
